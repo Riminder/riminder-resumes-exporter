9358dfd8e523dbfbd5ac3fcddfcf6194ebe27dcd
Alexandre Gotte <alexandre.gotte@riminder.net>
2018-07-09 17:36:41 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='resume_exporter',
+      version='0.0.1',
+      description='Riminder resume exporter.',
+      url='https://github.com/Riminder/riminder-resumes-exporter',
+      author='riminder',
+      author_email='contact@rimider.net',
+      license='MIT',
+      packages=['.'],
+      install_requires=[
+          'riminder==1.0.4'
+      ],
+      entry_points={
+        'console_scripts': [
+            'resumeExporter = resume_exporter:main',
+        ]
+      },
+      python_requires='>=3.5',
+      zip_safe=False)
