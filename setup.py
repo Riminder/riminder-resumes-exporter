a2f9751b823a5f42a4018d4413072b57009b732e
Georges B <gbergossi@gmail.com>
2019-06-10 20:59:54 +0200

    update setup version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='riminder_resume_exporter',
-      version='0.0.2',
+      version='0.0.3',
       description='Riminder resume exporter.',
       url='https://github.com/Riminder/riminder-resumes-exporter',
       author='riminder',
